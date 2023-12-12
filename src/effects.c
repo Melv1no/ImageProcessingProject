45b7df6267a70c8872baef646c8b50eb5346b4a1
Melvin OLIVET <olivet.melvin@gmail.com>
2023-12-12 17:15:33 +0100

    version qui marche bien avec le mirror effect

--- src/effects.c
+++ src/effects.c
@@ -24,6 +24,21 @@ void applyMirrorEffect() {
         return;
     }
 
-    // Implement the logic for applying the mirror effect
+    // Calculate the middle column
+    int middleColumn = loadedImage->width / 2;
+
+    // Iterate through each row and swap pixels across the middle column
+    for (int row = 0; row < loadedImage->height; ++row) {
+        for (int col = 0; col < middleColumn; ++col) {
+            int leftIndex = row * loadedImage->width + col;
+            int rightIndex = row * loadedImage->width + (loadedImage->width - 1 - col);
+
+            // Swap pixel values
+            unsigned char temp = loadedImage->data[leftIndex];
+            loadedImage->data[leftIndex] = loadedImage->data[rightIndex];
+            loadedImage->data[rightIndex] = temp;
+        }
+    }
+
     printf("Effect applied: Mirror\n");
 }
